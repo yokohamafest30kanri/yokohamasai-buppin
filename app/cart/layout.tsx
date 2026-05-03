49c574f220eba545e85544f471bc74bc50a57b8f
wakih <yokohamafest30.kanri@gmail.com>
2026-05-04 02:01:36 +0900

    add cart/layout

---
+++ app/cart/layout.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+export default function ItemsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {/* ===== items用ヘッダー ===== */}
+      <header
+        style={{    
+          position: "sticky",
+          top: 0,
+          zIndex: 1000,
+
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "12px 20px",
+          borderBottom: "1px solid #ccc",
+          marginBottom: "20px",
+          backgroundColor: "#e3f2fd",
+        }}
+      >
+        <h2 style={{ fontSize: "16px", fontWeight: "bold" }}>
+          30th横浜祭 借用物品登録サイト
+        </h2>
+
+      </header>
+
+      {/* ===== ページ本体 ===== */}
+      {children}
+    </>
+  );
+}
