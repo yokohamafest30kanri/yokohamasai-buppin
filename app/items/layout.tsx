c6e3666dd2d9afa0223ede0f95af97c04448d1e4
wakih <yokohamafest30.kanri@gmail.com>
2026-05-10 18:29:55 +0900

    edit layout

--- app/items/layout.tsx
+++ app/items/layout.tsx
@@ -3,15 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-
-export const metadata = {
-  robots: {
-    index: false,
-    follow: false,
-  },
-};
-
-
 export default function ItemsLayout({
   children,
 }: {
